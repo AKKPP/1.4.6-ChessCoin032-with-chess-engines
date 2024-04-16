10803a409df7037757657a41a59ffabe0cb9232e
OleksandrAAA <aleksandkolesnik73@gmail.com>
2024-04-16 23:06:44 +0700

    Fixed BitcoinGui closeEvent

--- chesscoin-qt/src/qt/bitcoingui.cpp
+++ chesscoin-qt/src/qt/bitcoingui.cpp
@@ -730,8 +730,8 @@ void BitcoinGUI::closeEvent(QCloseEvent *event)
     if(clientModel)
     {
 #ifndef Q_OS_MAC // Ignored on Mac
-        //if(!clientModel->getOptionsModel()->getMinimizeToTray() && !clientModel->getOptionsModel()->getMinimizeOnClose())
-        if (!clientModel->getOptionsModel()->getMinimizeOnClose())
+        if(!clientModel->getOptionsModel()->getMinimizeToTray() && !clientModel->getOptionsModel()->getMinimizeOnClose())
+        //if (!clientModel->getOptionsModel()->getMinimizeOnClose())
         {
             qApp->quit();
         }
